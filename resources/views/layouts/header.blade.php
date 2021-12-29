1ada0e1214c80ab8773ec0ae2eacb7337527354b
kenjikuroki <90098303+kenjikuroki@users.noreply.github.com>
2021-12-29 20:06:46 +0900

    sidebar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,19 @@
 @section('header')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<div class="header"></div>
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- Popper.JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<!-- jQuery Custom Scroller CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.concat.min.js"></script>
+
+<div class="header">
+  <link rel="stylesheet" href="sidebar.css">
+  <!-- Scrollbar Custom CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+
+</div>
 <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
 <label for="openSidebarMenu" class="sidebarIconToggle">
   <div class="spinner diagonal part-1"></div>
@@ -8,51 +21,88 @@
   <div class="spinner diagonal part-2"></div>
 </label>
 <div id="sidebarMenu">
+
   <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 mt-5">
-          <div class="card">
-            <div class="card-header">
-              <span class="mb-2">test</span>
-            </div>
-            <div class="card-body">
-              <div id="demo-area" class="form-group">
-                <div class="unit input-group mb-2">
-                  <input name="test[]" type="text">
-                  <div class="demo-minus input-group-append">
-                    <span class="btn btn-danger">-</span>
-                  </div>
-                </div>
-              </div>
-              <div id="demo-plus" class="btn btn-primary">+</div>
-            </div>
-          </div>
+    <div class="wrapper">
+      <!-- Sidebar -->
+      <nav id="sidebar">
+        <div class="sidebar-header">
+          <h3>Bootstrap Sidebar</h3>
         </div>
+
+        <ul class="list-unstyled components">
+          <p>Dummy Heading</p>
+          <li class="active">
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
+            <ul class="collapse list-unstyled" id="homeSubmenu">
+              <li>
+                <a href="#">Home 1</a>
+              </li>
+              <li>
+                <a href="#">Home 2</a>
+              </li>
+              <li>
+                <a href="#">Home 3</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
+            <ul class="collapse list-unstyled" id="pageSubmenu">
+              <li>
+                <a href="#">Page 1</a>
+              </li>
+              <li>
+                <a href="#">Page 2</a>
+              </li>
+              <li>
+                <a href="#">Page 3</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Portfolio</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+
+      </nav>
+      <!-- Page Content -->
+      <div id="content">
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+
+            <button type="button" id="sidebarCollapse" class="btn btn-info">
+              <i class="fas fa-align-left"></i>
+              <span>Toggle Sidebar</span>
+            </button>
+          </div>
+        </nav>
       </div>
     </div>
-
     <script type="text/javascript">
-      var minCount = 1;
-      var maxCount = 6;
-      $(function() {
-        $('#demo-plus').on('click', function() {
-          var inputCount = $('#demo-area .unit').length;
-          if (inputCount < maxCount) {
-            var element = $('#demo-area .unit:last-child').clone(true);
-            var inputList = element[0].querySelectorAll('input[type="text"]');
-            for (var i = 0; i < inputList.length; i++) {
-              inputList[i].value = "";
-            }
-            $('#demo-area .unit').parent().append(element);
-          }
+      $(document).ready(function() {
+
+        $("#sidebar").mCustomScrollbar({
+          theme: "minimal"
         });
-        $('.demo-minus').on('click', function() {
-          var inputCount = $('#demo-area .unit').length;
-          if (inputCount > minCount) {
-            $(this).parents('.unit').remove();
-          }
+
+        $('#sidebarCollapse').on('click', function() {
+          // open or close navbar
+          $('#sidebar').toggleClass('active');
+          // close dropdowns
+          $('.collapse.in').toggleClass('in');
+          // and also adjust aria-expanded attributes we use for the open/closed arrows
+          // in our CSS
+          $('a[aria-expanded=true]').attr('aria-expanded', 'false');
         });
+
       });
     </script>
   </body>
