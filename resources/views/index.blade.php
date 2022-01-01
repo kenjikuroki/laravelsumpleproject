860ac87968e048c9d46ef3ca8214a4d2b207c88a
kenjikuroki <90098303+kenjikuroki@users.noreply.github.com>
2022-01-01 13:59:41 +0900

    migrate

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,64 +10,78 @@
 @include('layouts.header')
 
 @section('content')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Simple Sidebar - Start Bootstrap Template</title>
+  <!-- aZFavicon-->
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/sidebar.css" rel="stylesheet" />
+</head>
 
 <body>
-  <div class="column">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 mt-5">
-          <div class="card">
-            <div class="card-header">
-              <span class="mb-2">TODOLIST</span>
-            </div>
-            <div class="card-body">
-              <div id="main-demo-area" class="form-group">
-                <div class="unit input-group mb-2">
-                  <input name="test[]" type="text">
-                  <div class="demo-minus input-group-append">
-                    <span class="btn btn-danger">-</span>
-                  </div>
-                </div>
-              </div>
-              <div id="main-demo-plus" class="btn btn-primary">+</div>
-            </div>
-          </div>
-        </div>
+  <div class="d-flex" id="wrapper">
+    <!-- Sidebar-->
+    <div class="border-end bg-white" id="sidebar-wrapper">
+      <div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
+      <div class="list-group list-group-flush">
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
       </div>
     </div>
-    <div class="demo_box">demo box</div>
-    <div class="demo_btn">クローンの作成</div>
-    <div id="demo_area"></div>
+    <!-- Page content wrapper-->
+    <div id="page-content-wrapper">
+      <!-- Top navigation-->
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container-fluid">
+          <button class="btn btn-primary" id="sidebarToggle">Toggle Menu</button>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+              <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#!">Action</a>
+                  <a class="dropdown-item" href="#!">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#!">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!-- Page content-->
+      <div class="container-fluid">
+        <h1 class="mt-4">Simple Sidebar</h1>
+        <p>The starting state of the menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will change.</p>
+        <p>
+          Make sure to keep all page content within the
+          <code>#page-content-wrapper</code>
+          . The top navbar is optional, and just for demonstration. Just create an element with the
+          <code>#sidebarToggle</code>
+          ID which will toggle the menu when clicked.
+        </p>
+      </div>
+    </div>
   </div>
-  <script type="text/javascript">
-    var minCount = 1;
-    var maxCount = 6;
-    $(function() {
-      $('#main-demo-plus').on('click', function() {
-        var inputCount = $('#main-demo-area .unit').length;
-        if (inputCount < maxCount) {
-          var element = $('#main-demo-area .unit:last-child').clone(true);
-          var inputList = element[0].querySelectorAll('input[type="text"]');
-          for (var i = 0; i < inputList.length; i++) {
-            inputList[i].value = "";
-          }
-          $('#main-demo-area .unit').parent().append(element);
-        }
-      });
-      $('.demo-minus').on('click', function() {
-        var inputCount = $('#main-demo-area .unit').length;
-        if (inputCount > minCount) {
-          $(this).parents('.unit').remove();
-        }
-      });
-    });
-    $('.demo_btn').on('click', function() {
-      $(".demo_box").eq(0).clone().appendTo("#demo_area");
-    });
-  </script>
+  <!-- Bootstrap core JS-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Core theme JS-->
+  
+  <script src=/js/sidebar.js></script>
 </body>
-@endsection
+@endsectiond
 
 
 @include('layouts.footer')
